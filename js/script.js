e5eb16775e0dd674f08f8441837bc60ee2c19b3a
Samsamb0 <samale903@gmail.com>
2023-02-16 15:57:30 +0100

    Creation of the computerPlay function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /*variables globales*/
 let player;
+let computer;
 function onload() {
   /*Initialisation de variables*/
   let rock = document.getElementById("rock");
@@ -15,4 +16,17 @@ function onload() {
   paper.addEventListener("click", function () {
     player = "paper";
   });
+}
+
+function computerPlay() {
+  let random = parseFloat((Math.random() * 100).toFixed(2));
+  if (random < 33.33) {
+    computer = "rock";
+  } else if (random >= 33.33 && random < 66.67) {
+    computer = "paper";
+  } else if (random >= 66.67) {
+    computer = "scissors";
+  } else {
+    console.log("BIG PROBLEM");
+  }
 }
