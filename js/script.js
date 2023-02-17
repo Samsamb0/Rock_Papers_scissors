013eeded807e828d404e02422fd53e61e4823a46
Samsamb0 <samale903@gmail.com>
2023-02-17 15:34:09 +0100

    Correction of the computerPlay() function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let player = "";
-
+let computer = "";
 /*variables globales*/
 
 function load() {
@@ -22,4 +22,21 @@ function load() {
     player = "scissors";
     playerEmote.innerHTML = "<img src='images/ciseaux.png'></img>";
   });
+}
+
+function computerPlay() {
+  /*initialisation of variables*/
+  let result = Number((Math.random() * 100).toFixed(2));
+  let computerEmote = document.getElementById("cEmote");
+  /*Event*/
+  if (result < 33.33) {
+    computer = "rock";
+    computerEmote.innerHTML = "<img src='images/pierre.png'></img>";
+  } else if (result >= 33.33 && result < 66.67) {
+    computer = "paper";
+    computerEmote.innerHTML = "<img src='images/feuille.png'></img>";
+  } else if (result > 66.67) {
+    computer = "scissors";
+    computerEmote.innerHTML = "<img src='images/ciseaux.png'></img>";
+  }
 }
